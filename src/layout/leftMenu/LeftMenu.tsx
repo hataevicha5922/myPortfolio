892179f5ef3e549ad56830baede85d0db037ab47
Aliaksandr <hataevicha5922@gmail.com>
2023-10-26 15:16:35 +0300

    layout

---
+++ src/layout/leftMenu/LeftMenu.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const LeftMenu = () => {
+  return <LeftMenuWrapper>Left Menu</LeftMenuWrapper>;
+};
+
+const LeftMenuWrapper = styled.aside`
+  height: 100vh;
+  background-color: beige;
+  width: 500px;
+`;
---
+++ src/layout/main/Main.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Main = () => {
+  return <MainWrapper>Main</MainWrapper>;
+};
+
+const MainWrapper = styled.main`
+  height: 100vh;
+  background-color: antiquewhite;
+`;
